38deb844cc838dd771c7b7b76539637bc5f70cc6
Plode <84858705+plode-v@users.noreply.github.com>
2024-02-23 12:04:53 +0700

    Update ExpressJS

--- server/index.js
+++ server/index.js
@@ -6,5 +6,9 @@ const app = express();
 app.use(bodyParser());
 
 app.get('/', (req, res) => {
-    
+    res.send("Hello world");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
 })
